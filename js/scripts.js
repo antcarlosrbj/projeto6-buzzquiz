fbbec1251e21b6675ae50d8a06334470389bc603
Estevam <estevamfurtado@gmail.com>
2022-02-17 00:31:09 -0300

    Ajustes na validacao de cor

--- js/scripts.js
+++ js/scripts.js
@@ -58,90 +58,6 @@ pullFromLocalStorage();
 refreshQuizzes();
 
 
-// Apagar
-const meuQuizz = {
-    title: "Meu primeiro quizz teste? Vamos lá?",
-    image: "https://imagens.brasil.elpais.com/resizer/A_AIZejFb4KXwd9h9fay-Ruft70=/414x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/WZF3MMB4ONHUTB5KBSMFZ7L2OI.JPG",
-    questions: [
-        {
-            title: "Qual o meu nome? Vamos lá!!!",
-            color: "#480372",
-            answers: [
-                {
-                    text: "Texto da resposta 1",
-                    image: "https://http.cat/411.jpg",
-                    isCorrectAnswer: true
-                },
-                {
-                    text: "Texto da resposta 2",
-                    image: "https://http.cat/412.jpg",
-                    isCorrectAnswer: false
-                },
-                {
-                    text: "Texto da resposta 2",
-                    image: "https://http.cat/412.jpg",
-                    isCorrectAnswer: false
-                }
-            ]
-        },
-        {
-            title: "Título da pergunta 2",
-            color: "#049824",
-            answers: [
-                {
-                    text: "Texto da resposta 1",
-                    image: "https://http.cat/411.jpg",
-                    isCorrectAnswer: true
-                },
-                {
-                    text: "Texto da resposta 2",
-                    image: "https://http.cat/412.jpg",
-                    isCorrectAnswer: false
-                },
-                {
-                    text: "Texto da resposta 2",
-                    image: "https://http.cat/412.jpg",
-                    isCorrectAnswer: false
-                }
-            ]
-        },
-        {
-            title: "Título da pergunta 3",
-            color: "#908432",
-            answers: [
-                {
-                    text: "Texto da resposta 1",
-                    image: "https://http.cat/411.jpg",
-                    isCorrectAnswer: true
-                },
-                {
-                    text: "Texto da resposta 2",
-                    image: "https://http.cat/412.jpg",
-                    isCorrectAnswer: false
-                },
-                {
-                    text: "Texto da resposta 2",
-                    image: "https://http.cat/412.jpg",
-                    isCorrectAnswer: false
-                }
-            ]
-        }
-    ],
-    levels: [
-        {
-            title: "HORRÍVEL HORRÍVEL",
-            image: "https://http.cat/411.jpg",
-            text: "Descrição do nível vamos lá tentar fazer bem mais",
-            minValue: 0
-        },
-        {
-            title: "TOP DO TOP DO TOP",
-            image: "https://http.cat/412.jpg",
-            text: "Descrição do nível vamos lá tentar fazer bem mais",
-            minValue: 50
-        }
-    ]
-}
 
 // --------- NAVEGAÇÃO -------------
 
@@ -246,6 +162,7 @@ function createQuizzThumbElement(quizz) {
 
     const element = document.createElement("div");
     element.classList.add("quizz");
+    element.id = quizz.id;
     element.innerHTML = `
         <div class="gradient"></div>
         <p>${quizz.title}</p>
@@ -287,7 +204,7 @@ function deleteLevelButtonClicked(index) {
 // Navegar entre seções
 function createNewQuizz() {
 
-    editingQuizz = newQuizz(); // meuQuizz
+    editingQuizz = newQuizz(); // newQuizz() meuQuizz meuQuizz2
     isEditingANewQuizz = true;
     editingQuizzIsValidated = false;
 
@@ -529,7 +446,7 @@ function isValidHttpUrl(string) {
 }
 
 function isValidColor(string) {
-    var reg = /^#([0-9a-f]{3}){1,2}$/i;
+    var reg = /^#([0-9a-f]{6}){1}$/i;
     return (reg.test(string));
 }
 
@@ -895,4 +812,136 @@ function newInputElement(fatherId, inp_name, tag, placeHolder, value) {
     element.innerHTML += `<p>Error</p>`;
 
     return element;
+}
+
+
+
+
+const meuQuizz2 = {
+    title: 'O quão carioca você é? Teste seus conhecimentos de cria!',
+    image: 'https://static.mundoeducacao.uol.com.br/mundoeducacao/2021/03/1-cristo-redentor.jpg',
+    questions: [
+        {
+            title: 'Qualquer perguntinha marota pq o teste é real', color: '#000000',
+            answers:[
+                {text: 'Rexposta certa de cria', isCorrectAnswer: true, image: 'https://i.pinimg.com/736x/56/4e/62/564e62fef96c4ef0d67a59c94b1a6288.jpg'},
+                {text: 'Resposta errada de nerd', isCorrectAnswer: false, image: 'https://psalm.escreveronline.com.br/wp-content/upl…014/12/bigstock-Shocked-Computer-Nerd-1520709.jpg'},
+                {text: 'Marola nao po ta maluco', isCorrectAnswer: false, image: 'https://psalm.escreveronline.com.br/wp-content/upl…014/12/bigstock-Shocked-Computer-Nerd-1520709.jpg'},
+                {text: 'Nerd loka bro pego a visao?', isCorrectAnswer: false, image: 'https://psalm.escreveronline.com.br/wp-content/upl…014/12/bigstock-Shocked-Computer-Nerd-1520709.jpg'}
+        ]},
+        {
+            title: 'Segunda perguntinha pra marolar...', color: '#000000',
+            answers:[
+                {text: 'Rexposta certa de cria', isCorrectAnswer: true, image: 'https://i.pinimg.com/736x/56/4e/62/564e62fef96c4ef0d67a59c94b1a6288.jpg'},
+                {text: 'Resposta errada de nerd', isCorrectAnswer: false, image: 'https://psalm.escreveronline.com.br/wp-content/upl…014/12/bigstock-Shocked-Computer-Nerd-1520709.jpg'},
+                {text: 'Marola nao po ta maluco', isCorrectAnswer: false, image: 'https://psalm.escreveronline.com.br/wp-content/upl…014/12/bigstock-Shocked-Computer-Nerd-1520709.jpg'},
+        ]},
+        {
+            title: 'Essa terceira é braba tá?', color: '#000000',
+            answers:[
+                {text: 'Rexposta certa de cria', isCorrectAnswer: true, image: 'https://i.pinimg.com/736x/56/4e/62/564e62fef96c4ef0d67a59c94b1a6288.jpg'},
+                {text: 'Resposta errada de nerd', isCorrectAnswer: false, image: 'https://psalm.escreveronline.com.br/wp-content/upl…014/12/bigstock-Shocked-Computer-Nerd-1520709.jpg'},
+                {text: 'Marola nao po ta maluco', isCorrectAnswer: false, image: 'https://psalm.escreveronline.com.br/wp-content/upl…014/12/bigstock-Shocked-Computer-Nerd-1520709.jpg'},
+        ]}
+    ],
+    levels: [
+        {
+            title: "100% CRIA das casinha",
+            text: "ta maluco po. vc é 100% cria irmaozinho. local dos local. norkreina doidao. fechamento patudo pego a visaozinha?",
+            image: "https://i.ytimg.com/vi/lM3d9f2AGoY/maxresdefault.jpg",
+            minValue: "100",
+        },
+        {
+            title: "nerd loka mao furada",
+            text: "ooooo pode saindo já. vc eh uma negacao tlg?? so seria pior se fose paulista. vacilao mete o pe",
+            image: "https://img.ibxk.com.br/2020/05/13/13105244489120.jpg",
+            minValue: "0",
+        }
+    ]
+}
+
+
+// Apagar
+const meuQuizz = {
+    title: "Meu primeiro quizz teste? Vamos lá?",
+    image: "https://imagens.brasil.elpais.com/resizer/A_AIZejFb4KXwd9h9fay-Ruft70=/414x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/WZF3MMB4ONHUTB5KBSMFZ7L2OI.JPG",
+    questions: [
+        {
+            title: "Qual o meu nome? Vamos lá!!!",
+            color: "#480372",
+            answers: [
+                {
+                    text: "Texto da resposta 1",
+                    image: "https://http.cat/411.jpg",
+                    isCorrectAnswer: true
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            title: "Título da pergunta 2",
+            color: "#049824",
+            answers: [
+                {
+                    text: "Texto da resposta 1",
+                    image: "https://http.cat/411.jpg",
+                    isCorrectAnswer: true
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            title: "Título da pergunta 3",
+            color: "#908432",
+            answers: [
+                {
+                    text: "Texto da resposta 1",
+                    image: "https://http.cat/411.jpg",
+                    isCorrectAnswer: true
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                }
+            ]
+        }
+    ],
+    levels: [
+        {
+            title: "HORRÍVEL HORRÍVEL",
+            image: "https://http.cat/411.jpg",
+            text: "Descrição do nível vamos lá tentar fazer bem mais",
+            minValue: 0
+        },
+        {
+            title: "TOP DO TOP DO TOP",
+            image: "https://http.cat/412.jpg",
+            text: "Descrição do nível vamos lá tentar fazer bem mais",
+            minValue: 50
+        }
+    ]
 }
